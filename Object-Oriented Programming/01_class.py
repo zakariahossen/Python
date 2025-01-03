a9b6f1ac6bd551810bcae3f0395e090029ed651f
MD Zakaria Hossen <113116138+zakariahossen@users.noreply.github.com>
2025-01-04 01:14:34 +0600

    Create 01_class.py

---
+++ Object-Oriented Programming/01_class.py
@@ -0,0 +1,6 @@
+class Car: #Creating The Class
+    brand = "Toyata" #Define Attributes
+    price = 980000
+    special = ['AC','Music','Maps','FM Radio']
+my_Car = Car() #Creating The Object
+print(my_Car.special) # Call the special Attribute
