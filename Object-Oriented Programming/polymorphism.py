65588734c2bd57e956cc7e23d01120f2f9ea5dd3
MD Zakaria Hossen <113116138+zakariahossen@users.noreply.github.com>
2025-01-07 13:01:59 +0600

    Create polymorphism.py

---
+++ Object-Oriented Programming/polymorphism.py
@@ -0,0 +1,20 @@
+ class Car:
+    car_name = "Wheeljack"
+    def move(self):
+        print(self.car_name, "Car is Moving")
+
+class Truck:
+    truck_name = "Optimus Prime"
+    def move(self):
+        print(self.truck_name, "Truck is moving")
+
+class Bike:
+    bike_name = "Elita-One"
+    def move(self):
+        print(self.bike_name, "Bike is moving")
+
+# List of vehicles
+vehicles = [Car(), Truck(), Bike()]
+
+for vehicle in vehicles:
+    vehicle.move()
