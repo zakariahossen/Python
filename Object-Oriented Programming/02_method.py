b2c50165b3899bd4375633fe5ab04c57984b21d5
MD Zakaria Hossen <113116138+zakariahossen@users.noreply.github.com>
2025-01-07 12:51:33 +0600

    Create 02_method.py

---
+++ Object-Oriented Programming/02_method.py
@@ -0,0 +1,15 @@
+class Car:
+    brand = "Farari"
+    def hard_work(self):
+        print("Race Race Race")
+        
+my_car = Car()
+my_car.hard_work()
+# Class 2
+class Calculator:
+    brand = "Walton"
+    def add (self, a,  b):
+        return a + b
+      
+my_calc = Calculator()
+print(my_calc.add (2,3))
