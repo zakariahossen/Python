5f5890cbd124170e1466338ea88d8df31376ec91
MD Zakaria Hossen <113116138+zakariahossen@users.noreply.github.com>
2025-01-19 14:48:32 +0000

    Create stack.py

---
+++ Data Structures/stack.py
@@ -0,0 +1,13 @@
+# This a exaple of Stack data structure
+heroes = ['Jhankar', 'Eshan','Tutul','Ali Hossain', 'Hero Alom'] # creating the stack named hero
+
+
+# Remove from Stack. You know, Stack follows LIFO(Last In Fast Out) policy, that means you have to remove the last element of the stack at first.
+heroes.pop()
+
+# Add new element in stack
+heroes.append('Rabbil Hasan') # Output: ['Jhankar', 'Eshan', 'Tutul', 'Ali Hossain', 'Rabbil Hasan']
+
+
+#print the stack
+print(heroes) # Output: ['Jhankar', 'Eshan', 'Tutul', 'Ali Hossain']
